cc03ef13338dd3ae380361b74300642c9005d5a0
siddhanthpx <siddhanth0@outlook.com>
2021-05-02 22:02:00 +0530

    initial commit

--- main.go
+++ main.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func main() {
-	dsn := "postgres://pdzenyaibpistf:00e77e74cf97e9e4fd9d647900a8e380a3bf132fcc48ca8927e2ca8b9680d9b4@ec2-54-166-167-192.compute-1.amazonaws.com:5432/db2vmap0n2c4of"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	dbURL := os.Getenv("DATABASE_URL")
+	db, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err)
 	}
